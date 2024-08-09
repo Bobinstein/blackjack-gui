3791b4757415db6a710e42a2d690b0fdf3b81b4a
bobinstein <cavanaugh.stephen1991@gmail.com>
2024-08-09 17:30:37 -0400

    feat: TRUNK description

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -22,10 +22,13 @@ export default function About() {
         <h1>Sponsors</h1>
         <ul>
           <li><a className={styles.hlink} href='https://sarcophagus.io/' target='_blank' >Sarcophagus.io</a></li>
+          <li><a className={styles.hlink} href='https://trunkao.xyz/#/' target='_blank'>TRUNK</a></li>
         </ul>
         <h2>About Sarcophagus.io</h2>
         <p>Sarcophagus is a decentralized dead man&apos;s switch application built on Ethereum and Arweave. It is a switch designed to be activated or deactivated if the human operator becomes incapacitated, such as through death, loss of consciousness, or being bodily removed from control.</p>
         <p>Sarcophagus sponsored AO Blackjack to help show off the power of ao.</p>
+        <h2>About Trunk</h2>
+        <p>$TRUNK is the very first meme token on AO/Arweave. $TRUNK is built on a meme frame which allows users to participate in DAO voting on chain on AO.</p>
       </main>
     </div>
   );
